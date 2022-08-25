1c7bc62d07cb66da5d8555f7c6274588f8c4a8aa
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-08-25 16:13:47 +0530

    file uploaded

---
+++ TwoDArray/twodarray.java
@@ -0,0 +1,28 @@
+package TwoDArray;
+import java.util.Scanner;
+
+public class twodarray {
+	public static void main(String[] args) {
+		
+		int a[][]=new int[2][2];
+		Scanner r=new Scanner(System.in);
+		System.out.print("Enter Array Elements ");
+		for(int i=0; i<2; i++) //rows
+		{
+			for(int j=0; j<2; j++) // columns
+			{
+				a[i][j]=r.nextInt();
+			}
+		}
+		System.out.print("Matrix: \n");
+		for(int i=0; i<2; i++) //rows
+		{
+			for(int j=0; j<2; j++) //columns
+			{
+				System.out.print(a[i][j]+" ");
+			}
+			System.out.print("\n");
+		}
+	}
+}
+

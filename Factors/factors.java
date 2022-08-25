d221c8eac862a863be02ecd6c89a54e9da4cadfe
Deepukumar1 <deepukumarmahasheth2@gmail.com>
2022-08-25 15:59:33 +0530

    file uploaded

---
+++ Factors/factors.java
@@ -0,0 +1,21 @@
+package Factors;
+
+import java.util.Scanner;
+
+public class factors {
+	public static void main(String[] args) {
+		int n;
+		System.out.print("Enter any Number ");
+		Scanner r=new Scanner(System.in);
+		n=r.nextInt();
+		
+		for(int i=1; i<=n; i++)
+		{
+			if(n%i==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+}
